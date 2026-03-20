6fe505e71e388fc123757b386560c00c4c50f2ea
Sreehari Jayachandran <sreeharijayachandran1@gmail.com>
2026-03-20 19:18:08 +0530

    Initial commit for mlmodel

--- backend/app/ml/loader.py
+++ backend/app/ml/loader.py
@@ -2,4 +2,4 @@ import joblib
 
 category_model = joblib.load("app/ml/models/categoryprediction.pkl")
 priority_model = joblib.load("app/ml/models/priorityprediction.pkl")
-vectorizer = joblib.load("app/ml/models/vectorizer.pkl")
+vectogit branchrizer = joblib.load("app/ml/models/vectorizer.pkl")
