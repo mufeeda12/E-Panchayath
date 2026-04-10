c4c7918b9869b2b4f7422e1752e55fd219016fe2
Sreehari Jayachandran <sreeharijayachandran1@gmail.com>
2026-04-10 20:01:08 +0530

    updated ml model vectorizer and functions

--- backend/app/ml/loader.py
+++ backend/app/ml/loader.py
@@ -1,5 +1,9 @@
 import joblib
 
-category_model = joblib.load("app/ml/models/categoryprediction.pkl")
-priority_model = joblib.load("app/ml/models/priorityprediction.pkl")
-vectogit branchrizer = joblib.load("app/ml/models/vectorizer.pkl")
+# Category
+category_model = joblib.load("app/ml/models/categoryprediction/category.pkl")
+category_vectorizer = joblib.load("app/ml/models/categoryprediction/cvectorizer.pkl")
+
+# Priority
+priority_model = joblib.load("app/ml/models/priorityprediction/priority.pkl")
+priority_vectorizer = joblib.load("app/ml/models/priorityprediction/pvectorizer.pkl")
