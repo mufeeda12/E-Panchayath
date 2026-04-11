42e59b75eb13c957d68fecb90ed21a36d4f39824
Sreehari Jayachandran <sreeharijayachandran1@gmail.com>
2026-04-11 13:45:19 +0530

    loader update

--- backend/app/ml/loader.py
+++ backend/app/ml/loader.py
@@ -1,8 +1,9 @@
 import joblib
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Category
+category_model = joblib.load("app/ml/models/categoryprediction/category.pkl")
+category_vectorizer = joblib.load("app/ml/models/categoryprediction/cvectorizer.pkl")
 
-category_model = joblib.load("app/ml/models/categoryprediction.pkl")
-priority_model = joblib.load("app/ml/models/priorityprediction.pkl")
-vectogit branchrizer = joblib.load("app/ml/models/vectorizer.pkl")
+# Priority
+priority_model = joblib.load("app/ml/models/priorityprediction/priority.pkl")
+priority_vectorizer = joblib.load("app/ml/models/priorityprediction/pvectorizer.pkl")
