e1b6196e54e1043b7b3f7afd4cd97858abf1f947
Sreehari Jayachandran <sreeharijayachandran1@gmail.com>
2026-05-18 19:38:47 +0530

    working model

--- backend/app/services/complaint_services.py
+++ backend/app/services/complaint_services.py
@@ -15,8 +15,8 @@ def create_complaint_services(db:Session,title:str,description:str,longitude:flo
     if not ward:
         raise HTTPException(status_code=400,detail="location is outside of boundaries")
     text=title+" "+description
-    category=predict_category(text)
-    priority=predict_priority(text)
+    category = int(predict_category(text))
+    priority = int(predict_priority(text))
 
     complaint= Complaint(
     title=title,
